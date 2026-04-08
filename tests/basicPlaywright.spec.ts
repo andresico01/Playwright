e81c5e1799eb02413e588920298f9bb5d0d73a17
andresico01 <andresrojasdg2.0@gmail.com>
2026-04-08 09:59:29 -0500

    create new example project

---
+++ tests/basicPlaywright.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect } from '@playwright/test';
+
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('');
+}  );
+
+test('How to interact with list of elements', async ({ page }) => {      
+    await expect(page.getByTestId('cta-primary')).toHaveCount(3);
+
+    // Get the count of elements with data-testid="cta-primary"
+    const count = await page.getByTestId('cta-primary').count();
+    console.log(`Number of elements with data-testid="cta-primary": ${count}`);
+
+    // Then we can interact with the elements, for example, click on the first one using nth(0)
+    await page.getByTestId('cta-primary').nth(0).click();
+
+     /* 
+    this implicitly waits is to can see the action in the video result, 
+    but in real test we should avoid it and use expect to wait for the element to be visible or hidden
+    */
+    await page.waitForTimeout(1000);
+    await page.getByTestId('close').click();
+
+   
+
+    // Or using first() method
+    await page.getByTestId('cta-primary').first().click();
+    /* 
+    this implicitly waits is to can see the action in the video result, 
+    but in real test we should avoid it and use expect to wait for the element to be visible or hidden
+    */
+    await page.waitForTimeout(1000);
+    await page.getByTestId('close').click();
+
+    
+    // Or used filer to click on the element with specific text
+    await page.getByTestId('cta-primary').filter({ hasText: 'Jugar gratis' }).click(); 
+    await page.getByTestId('close').click();
+    /* 
+    this implicitly waits is to can see the action in the video result, 
+    but in real test we should avoid it and use expect to wait for the element to be visible or hidden
+    */
+
+    await page.waitForTimeout(1000);
+
+    
+});
+
+test('How used expect and the diferente methods to do the validations', async ({ page }) => {
+    await expect(page.getByTestId('riotbar:desktopNav:link-internal-patch_notes')).toContainText('Notas de');
+
+    await expect(page.getByTestId('masthead-logo')).toBeVisible();       
+    
+    await expect(page.getByTestId('riotbar:desktopNav:link-internal-champions')).toHaveText('Campeones');
+
+    await expect(page.locator('//div[@data-testid="blade-content"]//span[text()="Akali"]')).toHaveCount(1);
+});
