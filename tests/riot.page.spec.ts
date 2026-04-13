098a980af4366773cc0b33a19e545fb0b321c087
andresico01 <andresrojasdg2.0@gmail.com>
2026-04-12 19:13:56 -0500

    remove selector

--- tests/riot.page.spec.ts
+++ tests/riot.page.spec.ts
@@ -11,7 +11,7 @@ test.describe('Test group', () => {
     const riotPage = new RiotPage(page);
     const homePage = new HomePage(page, isMobile);
     if (isMobile) {
-        await page.getByRole('button', { name: 'Aceptar todo' }).click();
+       // await page.getByRole('button', { name: 'Aceptar todo' }).click();
         await page.getByTestId('riotbar:mobile:menu:button-open').click();
     }
     const noticias = await homePage.getNoticiasGame();
